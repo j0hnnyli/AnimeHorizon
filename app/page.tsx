4ca7c99bf67715c647ab6dd2678c44ef8e833f2d
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-10-23 18:13:11 -0400

    homepage showcase and popular test

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-// import PopularsSection from "@/components/homepage/PopularsSection";
+import PopularsSection from "@/components/homepage/PopularsSection";
 // import FantasySection from "@/components/homepage/FantasySection";
 // import ActionSection from "@/components/homepage/ActionSection";
 // import HorrorSection from "@/components/homepage/HorrorSection";
 // import { HomePageData } from "@/lib/fetchFns";
-// import ShowCase from "@/components/homepage/ShowCase";
+import ShowCase from "@/components/homepage/ShowCase";
+
+import { getPopularsAnimes } from "@/lib/fetchFns";
 
 export default async  function Home() {
+  const {animes} = await getPopularsAnimes(1);
   // const {popular, fantasy, horror, action} = await HomePageData()
 
   return (
     <div className="max-w-[2000px] mx-auto">
-      home page
-      {/* <ShowCase animes={popular}/>
-      <PopularsSection popularAnimes={popular}/>
-      <FantasySection fantasyAnimes={fantasy}/>
+      <ShowCase animes={animes}/>
+      <PopularsSection popularAnimes={animes}/>
+      {/* <FantasySection fantasyAnimes={fantasy}/>
       <ActionSection actionAnimes={action} />
-      <HorrorSection horrorAnimes={horror}/> */}
+      <HorrorSection horrorAnimes={horror}/>  */}
     </div>
   );
 }
