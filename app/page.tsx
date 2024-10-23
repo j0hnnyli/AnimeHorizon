b0cf90eb96f7d9d743b30491b492ecec007012a6
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-10-23 18:02:33 -0400

    text homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import PopularsSection from "@/components/homepage/PopularsSection";
-import FantasySection from "@/components/homepage/FantasySection";
-import ActionSection from "@/components/homepage/ActionSection";
-import HorrorSection from "@/components/homepage/HorrorSection";
-import { HomePageData } from "@/lib/fetchFns";
-import ShowCase from "@/components/homepage/ShowCase";
+// import PopularsSection from "@/components/homepage/PopularsSection";
+// import FantasySection from "@/components/homepage/FantasySection";
+// import ActionSection from "@/components/homepage/ActionSection";
+// import HorrorSection from "@/components/homepage/HorrorSection";
+// import { HomePageData } from "@/lib/fetchFns";
+// import ShowCase from "@/components/homepage/ShowCase";
 
 export default async  function Home() {
   // const {popular, fantasy, horror, action} = await HomePageData()
