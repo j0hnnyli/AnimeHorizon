fd24844cc310ab07853df39fcf5232616c94cc1f
Johnny li <lijohnny21@gmail.com>
2025-02-23 14:16:44 -0500

    unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images : {
-    domains : ["cdn.myanimelist.net"]
+    domains : ["cdn.myanimelist.net"],
+    unoptimized: false
   }
 };
 
